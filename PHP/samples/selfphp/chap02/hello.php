4612d6a940087676c285a57a4b45906635f15e4d
ShouHorikawa <shou08062739@gmail.com>
2023-10-16 20:40:38 +0900

    PHP_2023_10_16 PHP第2章

---
+++ PHP/samples/selfphp/chap02/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHPの基本</title>
+    </head>
+    <body>
+        <?php
+        // print命令は指定した文字列を表示するための命令文です。
+        print 'こんにちは、世界!<br /> ';
+        print 'こんにちは、皆さん! ';
+        ?>
+    </body>
+</html>
