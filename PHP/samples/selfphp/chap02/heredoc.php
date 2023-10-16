05d9662ed847ffc1203fb5b9a9bb5c596a3c5d20
ShouHorikawa <shou08062739@gmail.com>
2023-10-16 22:31:31 +0900

    PHP_2023_10_16 独習PHP ヒアドキュメント <<<'EOD'

--- PHP/samples/selfphp/chap02/heredoc.php
+++ PHP/samples/selfphp/chap02/heredoc.php
@@ -1,6 +1,6 @@
 <?php
     $str = 'PHP(PHP: Hypertext Preprocessor)';
-    $msg = <<<EOD
+    $msg = <<<'EOD'
     {$str}は、サーバーサイドで動作する簡易なスクリプト言語です。
     まずは、本書でじっくり基礎固めしましょう。<br />
     "Let's start, everyone!!"
